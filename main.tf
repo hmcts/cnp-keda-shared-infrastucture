99f8ac03ebe8b8f959fcc20f939f4b4f8ae8b9ee
msl8r <72858053+msl8r@users.noreply.github.com>
2022-06-22 17:55:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-locals {
-  env = var.env == "sandbox" ? "sbox" : var.env
-}
-
 resource "azurerm_user_assigned_identity" "managed_identity" {
 
   resource_group_name = "managed-identities-${var.env}-rg"
   location            = var.location
 
-  name = "${var.product}-${local.env}-mi"
+  name = "${var.product}-${var.env}-mi"
 
   tags = var.common_tags
 }
