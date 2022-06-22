c0f2e18dd6c43b38477699f006035a063259554a
msl8r <72858053+msl8r@users.noreply.github.com>
2022-06-22 17:47:42 +0100

    Fix name for keda mi in sandbox environment

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+locals {
+  env = var.env == "sandbox" ? "sbox" : var.env
+}
+
 resource "azurerm_user_assigned_identity" "managed_identity" {
 
   resource_group_name = "managed-identities-${var.env}-rg"
   location            = var.location
 
-  name = "${var.product}-${var.env}-mi"
+  name = "${var.product}-${local.env}-mi"
 
   tags = var.common_tags
 }
